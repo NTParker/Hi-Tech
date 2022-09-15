50c63657ed91267240cac83acd1891c7dfa3f516
Noah Parker <noahtmparker@gmail.com>
2022-09-15 16:40:12 -0400

    updated post to match mock up

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,7 +6,7 @@ const { Post, User, Comment } = require("../../models");
 router.get("/", (req, res) => {
   console.log("======================");
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: ["id", "title", "content", "created_at"],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -35,7 +35,7 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: ["id", "title", "content", "created_at"],
     include: [
       {
         model: Comment,
@@ -68,7 +68,7 @@ router.post("/", (req, res) => {
   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
   Post.create({
     title: req.body.title,
-    post_url: req.body.post_url,
+    content: req.body.content,
     user_id: req.body.user_id,
   })
     .then((dbPostData) => res.json(dbPostData))
